c45300ca6e4e86caa223915fca9e272de2989580
marvin255 <postbox433@gmail.com>
2021-10-21 10:07:30 +0200

    setup commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ferone API Connector
---
+++ tests/BaseTestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SuareSu\FeroneApiConnector\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+abstract class BaseTestCase extends TestCase
+{
+}
