07e466189e2fccb7b360fadc972ce547e5fe5dd9
marvin255 <postbox433@gmail.com>
2021-10-24 21:08:48 +0200

    Add logger (#23)

--- tests/Transport/TransportFactoryTest.php
+++ tests/Transport/TransportFactoryTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SuareSu\FeroneApiConnector\Tests\Transport;
 
 use GuzzleHttp\Client;
+use Psr\Log\LoggerInterface;
 use SuareSu\FeroneApiConnector\Tests\BaseTestCase;
 use SuareSu\FeroneApiConnector\Transport\Transport;
 use SuareSu\FeroneApiConnector\Transport\TransportFactory;
@@ -22,10 +23,12 @@ class TransportFactoryTest extends BaseTestCase
         $url = 'http://test.ru';
         $authKey = 'test';
         $client = $this->getMockBuilder(Client::class)->disableOriginalConstructor()->getMock();
+        $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
         $transport = TransportFactory::new()
             ->setUrl($url)
             ->setAuthKey($authKey)
+            ->setLogger($logger)
             ->createForGuzzleClient($client);
 
         $this->assertInstanceOf(Transport::class, $transport);
