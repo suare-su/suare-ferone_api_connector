d6394e5c9409dca99ce82fe11723625746213d98
marvin255 <postbox433@gmail.com>
2022-02-25 22:42:15 +0100

    Fix parameter in method

--- src/Query/CheckAddressInCityZoneQuery.php
+++ src/Query/CheckAddressInCityZoneQuery.php
@@ -7,6 +7,32 @@ namespace SuareSu\FeroneApiConnector\Query;
 /**
  * Query object for CheckAddressInCityZone.
  */
-class CheckAddressInCityZoneQuery extends CheckAddressInZonesQuery
+class CheckAddressInCityZoneQuery extends AbstractQuery
 {
+    public const PARAM_CITY_ID = 'CityID';
+    public const PARAM_ADDRESS = 'Address';
+
+    /**
+     * Set CityID parameter.
+     *
+     * @param int $value
+     *
+     * @return $this
+     */
+    public function setCityId(int $value): self
+    {
+        return $this->add(self::PARAM_CITY_ID, $value);
+    }
+
+    /**
+     * Set Address parameter.
+     *
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function setAddress(string $value): self
+    {
+        return $this->add(self::PARAM_ADDRESS, $value);
+    }
 }
