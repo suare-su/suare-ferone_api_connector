e02798f3c41592901678f8c9812138761da12b2e
marvin255 <postbox433@gmail.com>
2023-05-31 21:08:00 +0200

    Fix ApiException::create

--- tests/Exception/ApiExceptionTest.php
+++ tests/Exception/ApiExceptionTest.php
@@ -19,11 +19,43 @@ class ApiExceptionTest extends BaseTestCase
     public function testCreate(): void
     {
         $expectedMessage = 'test message';
+        $expectedCode = 123;
         $expectedResponse = new TransportResponse([]);
 
-        $exception = ApiException::create($expectedMessage, $expectedResponse);
+        $exception = ApiException::create($expectedMessage, $expectedCode, $expectedResponse);
 
         $this->assertSame($expectedMessage, $exception->getMessage());
+        $this->assertSame($expectedCode, $exception->getCode());
+        $this->assertSame($expectedResponse, $exception->getResponse());
+    }
+
+    /**
+     * @test
+     */
+    public function testCreateNoResponse(): void
+    {
+        $expectedMessage = 'test message';
+        $expectedCode = 123;
+
+        $exception = ApiException::create($expectedMessage, $expectedCode);
+
+        $this->assertSame($expectedMessage, $exception->getMessage());
+        $this->assertSame($expectedCode, $exception->getCode());
+        $this->assertNull($exception->getResponse());
+    }
+
+    /**
+     * @test
+     */
+    public function testCreateNoCode(): void
+    {
+        $expectedMessage = 'test message';
+        $expectedResponse = new TransportResponse([]);
+
+        $exception = ApiException::create($expectedMessage, null, $expectedResponse);
+
+        $this->assertSame($expectedMessage, $exception->getMessage());
+        $this->assertSame(0, $exception->getCode());
         $this->assertSame($expectedResponse, $exception->getResponse());
     }
 
